2a6af5d19f2fc4d25470db4249d0e2ee231fb40a
Vedant102 <jadhavvedant102@gmail.com>
2021-09-28 18:56:19 +0530

    DATATYPE IN JS

---
+++ app.js
@@ -0,0 +1,13 @@
+//string
+console.log(typeof "hello")
+console.log(typeof 'v')
+
+//Number
+console.log(typeof 102)
+console.log(typeof 2.5)
+console.log(typeof -33)
+console.log(typeof -3.333)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
