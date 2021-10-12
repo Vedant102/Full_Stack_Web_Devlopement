a49c080f3b61892b5097d3d27676a7fcd28c812e
Vedant102 <jadhavvedant102@gmail.com>
2021-10-12 11:56:43 +0530

    Local storage

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const {add, subtract, multiply, divide} = require('./utils.js')
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+div.innerHTML = localStorage.getItem('inputValue')
 
-add()
-subtract()
-multiply()
-divide()
+button.addEventListener('click', (req, res) => {
+  localStorage.setItem('inputValue', input.value)
+  div.innerHTML = input.value
+  
+})
