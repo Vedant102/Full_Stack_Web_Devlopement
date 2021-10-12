4e7253728a41cef83a4ea40478c4d35a26ffb729
Vedant102 <jadhavvedant102@gmail.com>
2021-10-12 19:59:09 +0530

    Server using ExpressJs

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
 
-const server = http.createServer((request, response) => {
-    //console.log(request) //url, headers, method
-    const markup= fs.readFileSync(path.resolve('./index.html'))
-    response.write(markup)
-    response.end()
+// const server = http.createServer((request, response) => {
+//     // request.url
+//     const { url } = request
+//     if (url == '/login') {
+//         response.write('<h1>LOGIN</h1>')
+//         response.end()
+//     }
+//     if (url == '/signup') {
+//         response.write('<h1>SINGUP</h1>')
+//         response.end()
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log(`Server listening at PORT: ${3000}`)
+// })
+
+const { response } = require('express')
+const express = require ('express')
+const app = express()
+
+app.get('/login',(req,res) =>{
+  res.send('login')
+})
+app.get('/signup',(req,res) =>{
+
+  res.send('sign up')
 })
 
-
-server.listen(3000, () => {
-    console.log(`Server listening at PORT: ${3000}`)
+app.listen(3000,() =>{
+  console.log("server listning at port 3000")
 })
