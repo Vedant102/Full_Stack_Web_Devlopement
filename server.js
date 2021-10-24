c4c9248b74aac822c2ef7224569a3639ecb9414b
Vedant102 <jadhavvedant102@gmail.com>
2021-10-24 16:05:24 +0530

    simple validator

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ app.use(express.json())
 
 let products = [{name: "iphone 12", price: "999"}, {name: "iphone 1", price: "1999"}, {name: "iphone 14", price: "2999"},] //array 
 
+//--------------PUBLIC ROUTES-------
 
-// GET Route 
 //send all products
 app.get('/products', (req, res)=>{  //products route
-  res.json({products})
+  res.json({products}) 
+})
+
+//-----------PRIVATE ROUTES----------
+
+app.post('/products/add', (req,res) =>{
+const{name,price} = req.body
+
+if (!name || !price) 
+  res.json({Error: "Validation Failed" })
+else{
+  products.push({
+    name,
+    price,
+    })
+res.send({products})  
+  }
 })
 
 app.listen(PORT,()=>{
