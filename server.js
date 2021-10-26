0e25864de04df1f1f568cf020b43cc37413b9f31
Vedant102 <jadhavvedant102@gmail.com>
2021-10-26 12:09:11 +0530

    Express Routes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require ('express')
 const database = require ('./database/db')
 const app = express()
+const categoryRoutes = require('./routes/categoryRoutes')
+
+console.log(categoryRoutes.stack)
+
 app.use(express.json())
+app.use('/', categoryRoutes)
 
+app.get('/', (req,res) => {
+    try {
+        console.log(document)
+    } catch (error) {
+       res.status(203).send(error.message) 
+    }
+})
 
-
-app.listen(3001, () =>{
+app.listen(3001, () => {
     console.log("listning to port 3001")
 })
