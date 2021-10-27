a6984858e976836aadf02c948ddc7e2304ba0419
Vedant102 <jadhavvedant102@gmail.com>
2021-10-27 16:23:57 +0530

    Delete route

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,8 +3,13 @@ const express = require('express')
 const router = express.Router()
 const { v4: uuidv4 } = require('uuid');
 
-router.get('/all', (req, res) => {
+/*
+    ALL
+ROUTE : http://localhost:3001/category/all
+METHOD : get
+*/
 
+router.get('/all', (req, res) => {
     try {
         res.status(200).json({
             categories: database.categories,
@@ -21,14 +26,17 @@ router.get('/all', (req, res) => {
 
 })
 
-
+/*
+    ADD REQUEST
+ROUTE : http://localhost:3001/category/add
+METHOD : post
+*/
 router.post('/add', (req, res) => {
   const { name } = req.body
   const newCategory = {
       id: uuidv4(),
       name
   }
-
   try {
       let includes = database.categories.find(item => item.name === name)
       if (!includes) database.categories.push(newCategory)
@@ -49,5 +57,36 @@ router.post('/add', (req, res) => {
   }
 
 })
+
+/*
+    DELETE REQUEST
+ROUTE : http://localhost:3001/category/add
+METHOD : post
+*/
+
+router.delete('/delete',(req, res) =>{
+    try {
+        const {id} = req.body //we can use "params" here to give parameter and delete element
+        //   let element = database.category.find(item=>item.id === id)
+        //   const index = database.categories.indexOf(element)
+        //   database.categories.splice(index,1)  
+        const newCategories = database.categories.filter(item=>item.id!==id)
+        database.categories = newCategories
+
+        res.status(200).json({
+            categories: newCategories,
+            message: "Successfully deleted category",
+            status: "SUCCESS"
+        })
+    } catch (error) {
+        res.status(200).json({
+            categories: [],
+            message: error.message,
+            status: "FAILED"
+        })
+    }
+
+})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 // Router is just an object which allows us to create routes
 // http://localhost:3001/1234/CategoryRoutes
 
-app.use('/', categoryRoutes) //Using the router
+app.use('/category', categoryRoutes) //Using the router
 
 app.listen(3001, () => {
     console.log("listning to port 3001")
